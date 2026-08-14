fe840ba3489ac27167fcbb6f1ec8bf99c8ee5e4c
DavidEvangelistaC99 <david.evangelista.c@gmail.com>
2026-08-14 15:28:58 -0500

    Delete schain

--- schain/schainpy/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Signal chain python package"""
-
-try:
-    from schainpy.controller import Project
-except:
-    pass
-
-__version__ = '3.0.1rc1'
--- schain/schainpy/model/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .graphics import *
-from .data import *
-from .io import *
-from .proc import *
-from .utils import *
